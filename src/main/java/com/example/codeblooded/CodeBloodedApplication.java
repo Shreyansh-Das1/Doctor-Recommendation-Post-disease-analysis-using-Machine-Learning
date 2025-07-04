a6a8db0b4f4f9960f244cb54469d6f62ffd69ef3
Shreyansh Das <dasshreyansh04@gmail.com>
2025-07-04 12:24:03 +0530

    First commit

---
+++ src/main/java/com/example/codeblooded/CodeBloodedApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeblooded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBloodedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBloodedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codeblooded/CodeBloodedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeblooded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBloodedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
